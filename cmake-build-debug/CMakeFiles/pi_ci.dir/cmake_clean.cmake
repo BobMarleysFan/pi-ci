9dc092ca41759cc076987f972ab6fdda152b855e
Alexander Sanochkin <player_number4@mail.ru>
2020-10-21 15:35:51 +0500

    Fix CMake for sure

---
+++ cmake-build-debug/CMakeFiles/pi_ci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pi_ci.dir/main.cpp.obj"
+  "libpi_ci.dll.a"
+  "pi_ci.exe"
+  "pi_ci.exe.manifest"
+  "pi_ci.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pi_ci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
